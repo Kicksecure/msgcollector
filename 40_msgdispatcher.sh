663450435662157158abbbacd37b90c49e1cf88f
Patrick Schleizer <adrelanos@riseup.net>
2014-04-06 19:04:01 +0000

    initial commit

---
+++ 40_msgdispatcher.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## This file is part of Whonix.
+## Copyright (C) 2012 - 2014 Patrick Schleizer <adrelanos@riseup.net>
+## See the file COPYING for copying conditions.
+
+## /etc/profile.d/40_msgdispatcher.sh
+
+if [ ! "$(tty)" = "/dev/tty1" ]; then
+   return 0
+else
+   /usr/lib/msgcollector/msgdispatcher_profile_d
+fi
+
+## End of /etc/profile.d/40_msgdispatcher.sh
