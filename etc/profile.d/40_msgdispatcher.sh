ecaabfef1ffc132286194eda8373026ad9031725
Patrick Schleizer <adrelanos@whonix.org>
2023-10-12 12:06:22 -0400

    refactoring

--- etc/profile.d/40_msgdispatcher.sh
+++ etc/profile.d/40_msgdispatcher.sh
@@ -1,14 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 
 ## Copyright (C) 2012 - 2023 ENCRYPTED SUPPORT LP <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
 
 ## /etc/profile.d/40_msgdispatcher.sh
 
-if [ ! "$(tty)" = "/dev/tty1" ]; then
+if [ ! "$XDG_SESSION_TYPE" = "tty" ]; then
+   ## Skip this if not run in tty.
    return 0
-else
-   /usr/libexec/msgcollector/msgdispatcher_profile_d
 fi
+
+## Do this only in /dev/tty1.
+if [ ! "$(tty)" = "/dev/tty1" ]; then
+   ## Not running in /dev/tty1.
+   return 0
+fi
+
+## Running in /dev/tty1.
+/usr/libexec/msgcollector/msgdispatcher_profile_d
 
 ## End of /etc/profile.d/40_msgdispatcher.sh
